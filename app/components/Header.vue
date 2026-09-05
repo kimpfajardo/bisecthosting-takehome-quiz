cf3c8647ccd689d127507afbc9972dd79552c1b6
Kim <67940664+kimpfajardo@users.noreply.github.com>
2026-09-05 22:50:26 +0800

    fix header font styling

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,10 +1,13 @@
 <template>
   <header class="text-white max-w-[1382px] mx-auto px-[16px] py-[10px] w-full flex items-center justify-between">
-    <div class="flex items-center">
+    <div class="flex flex-wrap items-center gap-x-6 gap-y-4">
       <img src="/bisect-logo.webp" alt="BisectHosting Logo" width="132" height="44"
         class="w-[132px] h-[44px] object-contain" />
-      <ul>
+      <ul class="flex flex-wrap items-center uppercase gap-x-12 gap-y-3">
         <NavItem label="Game Servers" />
+        <NavItem label="Other Servers" />
+        <NavItem label="Learn" />
+        <NavItem label="Support" />
       </ul>
     </div>
     <slot />
--- app/components/NavItem.vue
+++ app/components/NavItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="flex items-center gap-[5px] cursor-pointer">
+  <li class="flex items-center gap-[5px] cursor-pointer font-semibold">
     <NuxtLink v-if="to" :to="to" class="flex items-center gap-[5px]">
       <slot name="prefix" />
       <slot>
@@ -12,12 +12,10 @@
           height="5"
           viewBox="0 0 7 5"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M3.0311 4.53279L6.06219 1.39158e-06L8.78972e-06 9.85114e-07L3.0311 4.53279Z"
-            :fill="arrowColor"
-          />
+            :fill="arrowColor" />
         </svg>
       </slot>
     </NuxtLink>
@@ -33,12 +31,10 @@
           height="5"
           viewBox="0 0 7 5"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M3.0311 4.53279L6.06219 1.39158e-06L8.78972e-06 9.85114e-07L3.0311 4.53279Z"
-            :fill="arrowColor"
-          />
+            :fill="arrowColor" />
         </svg>
       </slot>
     </a>
@@ -54,12 +50,10 @@
           height="5"
           viewBox="0 0 7 5"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M3.0311 4.53279L6.06219 1.39158e-06L8.78972e-06 9.85114e-07L3.0311 4.53279Z"
-            :fill="arrowColor"
-          />
+            :fill="arrowColor" />
         </svg>
       </slot>
     </template>
@@ -68,18 +62,18 @@
 
 <script setup lang="ts">
 interface Props {
-  label?: string
-  to?: string
-  href?: string
-  hasDropdown?: boolean
-  arrowColor?: string
+  label?: string;
+  to?: string;
+  href?: string;
+  hasDropdown?: boolean;
+  arrowColor?: string;
 }
 
 withDefaults(defineProps<Props>(), {
-  label: '',
-  to: '',
-  href: '',
+  label: "",
+  to: "",
+  href: "",
   hasDropdown: true,
-  arrowColor: '#03DDFF',
-})
+  arrowColor: "#03DDFF",
+});
 </script>
