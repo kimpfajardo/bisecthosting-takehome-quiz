8000d51c1dd0a57d9fd09496457c501366407a97
Kim <67940664+kimpfajardo@users.noreply.github.com>
2026-09-07 19:36:10 +0800

    create banner component and add akira font

---
+++ app/components/Banner.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="absolute top-[141px] left-[212px] inline-flex h-[71px] w-[1216px] items-center gap-[189px] rounded border border-white bg-[radial-gradient(50%_50%_at_50%_50%,_#B855E7_0%,_#B739F2_100%)] pl-[27px] pr-[20px] pt-[11px] pb-[10px]"
+    v-bind="$attrs">
+    <slot />
+  </div>
+</template>
