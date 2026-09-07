6fd64d2776272351c06b13d33ef7af410f0d85a3
Kim <67940664+kimpfajardo@users.noreply.github.com>
2026-09-07 18:35:18 +0800

    create chip component

---
+++ app/components/Chip.vue
@@ -0,0 +1,7 @@
+<template>
+  <span
+    class="inline-flex h-[34px] w-[231px] items-center justify-center gap-2.5 rounded-full border border-white bg-[linear-gradient(0deg,_var(--color-dark-300,_#12183B),_var(--color-dark-300,_#12183B)),linear-gradient(0deg,_rgba(183,57,242,0.1)_0%,_rgba(183,57,242,0)_100%)] px-4 py-1 text-white"
+    v-bind="$attrs">
+    <slot />
+  </span>
+</template>
